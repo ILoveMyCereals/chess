8507fe7f88c14004fa3900acb2365ab82e42d78c
Nathan Smith <eldernathan99@gmail.com>
2024-05-15 21:07:50 -0600

    Implemented getter and setter for teamTurn

--- shared/src/main/java/chess/ChessGame.java
+++ shared/src/main/java/chess/ChessGame.java
@@ -11,6 +11,7 @@ import java.util.Collection;
  */
 public class ChessGame {
     private ChessBoard board;
+    private TeamColor color1;
 
     public ChessGame() {
         board = new ChessBoard();
@@ -20,7 +21,7 @@ public class ChessGame {
      * @return Which team's turn it is
      */
     public TeamColor getTeamTurn() {
-        throw new RuntimeException("Not implemented");
+        return color1;
     }
 
     /**
@@ -29,7 +30,7 @@ public class ChessGame {
      * @param team the team whose turn it is
      */
     public void setTeamTurn(TeamColor team) {
-        throw new RuntimeException("Not implemented");
+        color1 = team;
     }
 
     /**
