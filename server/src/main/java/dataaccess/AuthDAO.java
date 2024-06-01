0cec2b0d80d4e8f2ca105216ec666527782bd828
Nathan Smith <eldernathan99@gmail.com>
2024-05-31 23:01:36 -0600

    Implemented createAuth and verifyAuth methods

--- server/src/main/java/dataaccess/AuthDAO.java
+++ server/src/main/java/dataaccess/AuthDAO.java
@@ -1,12 +1,14 @@
 package dataaccess;
 
 import model.AuthData;
+import java.sql.Connection;
+import java.sql.SQLException;
 
 public interface AuthDAO {
 
-    String createAuth(String username);
+    String createAuth(Connection conn, String username) throws SQLException;
 
-    AuthData verifyAuth(String authToken);
+    String verifyAuth(Connection conn, String authToken) throws SQLException;
 
-    void deleteAuth(String authToken);
+    void deleteAuth(Connection conn, String authToken) throws SQLException;
 }
--- server/src/main/java/dataaccess/SQLDAO/SQLAuthDAO.java
+++ server/src/main/java/dataaccess/SQLDAO/SQLAuthDAO.java
@@ -2,22 +2,36 @@ package dataaccess.SQLDAO;
 
 import dataaccess.AuthDAO;
 import model.AuthData;
+import java.sql.Connection;
+import java.sql.SQLException;
+import java.util.UUID;
 
 public class SQLAuthDAO implements AuthDAO {
 
-    public String createAuth(String username) {
-        return null;
+    public String createAuth(Connection conn, String givenName) throws SQLException {
+        String newAuth = UUID.randomUUID().toString();
+        try (var preparedStatement = conn.prepareStatement("INSERT INTO chess (auth) VALUES(?, ?)")) {
+            preparedStatement.setString(1, givenName);
+            preparedStatement.setString(2, newAuth);
+            preparedStatement.executeUpdate();
+        }
+        return newAuth;
     }
 
-    public AuthData verifyAuth(String authToken) {
-        return null;
+    public String verifyAuth(Connection conn, String givenToken) throws SQLException {
+        try (var preparedStatement = conn.prepareStatement("SELECT authToken, FROM auth, WHERE givenToken=?")) {
+            preparedStatement.setString(1, givenToken);
+            var queryResult = preparedStatement.executeQuery();
+            var passResult = queryResult.getString("authToken");
+            return passResult;
+        }
     }
 
-    public void deleteAuth(String authToken) {
+    public void deleteAuth(Connection conn, String authToken) throws SQLException {
         return;
     }
 
-    public void clearAuths() {
+    public void clearAuths(Connection conn) throws SQLException {
         return;
     }
 }
