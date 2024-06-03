e7a85ea50fdb87f423808e34c6b28e50e37645bc
Nathan Smith <eldernathan99@gmail.com>
2024-06-03 17:19:15 -0600

    Implemented listGames and setTeamUser methods

--- server/src/main/java/dataaccess/SQLDAO/SQLGameDAO.java
+++ server/src/main/java/dataaccess/SQLDAO/SQLGameDAO.java
@@ -1,6 +1,8 @@
 package dataaccess.SQLDAO;
 
 import java.util.ArrayList;
+
+import Handlers.ConvertJSON;
 import dataaccess.GameDAO;
 import model.GameData;
 import java.sql.Connection;
@@ -13,7 +15,23 @@ import com.google.gson.Gson;
 public class SQLGameDAO implements GameDAO {
 
     public ArrayList<GameData> listGames(Connection conn) throws SQLException {
-        return null;
+        ArrayList<GameData> games = new ArrayList<>();
+        try (var preparedStatement = conn.prepareStatement("SELECT gameName, gameID, blackUsername, whiteUsername, game")) {
+            try (var query = preparedStatement.executeQuery()) {
+                while(query.next()) {
+                    var gameName = query.getString("gameName");
+                    var gameID = query.getInt("gameID");
+                    var blackUsername = query.getString("blackUsername");
+                    var whiteUsername = query.getString("whiteUsername");
+
+                    var json = query.getString("game");
+                    var chessGame = ConvertJSON.fromJSON(json, ChessGame.class);
+
+                    games.add(new GameData(gameID, whiteUsername, blackUsername, gameName, chessGame));
+                }
+            }
+        }
+        return games;
     }
 
     public Integer createGame(Connection conn, String givenName) throws SQLException {
@@ -37,7 +55,17 @@ public class SQLGameDAO implements GameDAO {
         }
     }
 
-    public boolean setTeamUser(Connection conn, GameData game, String username, String teamColor) throws SQLException {
-        return false;
+    public boolean setTeamUser(Connection conn, GameData game, String givenName, String givenColor) throws SQLException {
+        if (givenColor == "WHITE" && game.getWhiteUsername() == null || givenColor == "BLACK" && game.getBlackUsername() == null) {
+            try (var preparedStatement = conn.prepareStatement("UPDATE chess SET teamColor=? WHERE gameID=?")) {
+                preparedStatement.setString(1, givenColor);
+                preparedStatement.setInt(2, game.getGameID());
+                preparedStatement.executeUpdate();
+                return true;
+            }
+        }
+        else {
+            return false;
+        }
     }
 }
